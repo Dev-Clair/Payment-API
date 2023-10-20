161b441f2cd0e6315b56e7faa3313476a5ec62b1
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-10-20 22:21:11 +0100

    fix: cli-config

--- cli-config.php
+++ cli-config.php
@@ -3,10 +3,11 @@
 use DI\Container;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use Doctrine\ORM\Tools\Console\EntityManagerProvider\SingleManagerProvider;
 
 /**
  * @var Container $container
  */
 $container = require_once __DIR__ . '/container/container.php';
 
-return ConsoleRunner::run($container->get(EntityManager::class));
+return ConsoleRunner::run(new SingleManagerProvider($container->get(EntityManager::class)));
