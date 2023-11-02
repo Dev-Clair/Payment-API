dfb403a07426836fcdbdcf711e79b241269a544e
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-11-02 16:39:07 +0100

    feat: modified auth middleware

--- src/Middleware/AuthMiddleware.php
+++ src/Middleware/AuthMiddleware.php
@@ -4,37 +4,48 @@ declare(strict_types=1);
 
 namespace Payment_API\Middleware;
 
-use Payment_API\Controller\AuthController;
-use Slim\Psr7\Response as Response;
+use Slim\Psr7\Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as Handler;
-use Payment_API\Utils\Response\Status_401 as Status_401;
+use Payment_API\Controller\AuthController;
 use Payment_API\Enums\MiddlewareResponseTitle as ResponseTitle;
+use Payment_API\Utils\Response\Status_401;
 
 class AuthMiddleware
 {
     use Status_401;
 
+    private AuthController $authController;
+
     public function __construct(private string $jwtSecretKey)
     {
+        $this->authController = new AuthController($this->jwtSecretKey);
     }
 
     public function __invoke(Request $request, Handler $handler): Response
     {
-        $authToken = $request->getHeaderLine('Authorization');
-        $authToken = str_replace('Bearer ', '', $authToken);
+        $authToken = $this->extractAuthToken($request);
 
         try {
-            (new AuthController($this->jwtSecretKey))->decode($authToken);
+            $this->authController->decode($authToken);
             return $handler->handle($request);
         } catch (\Exception $e) {
-            return $this->status_401(
-                ResponseTitle::UNAUTHORIZED,
-                [
-                    'Cannot retrieve resource' => $e->getMessage()
-                ],
-                'Kindly provide a valid bearer token'
-            );
+            return $this->handleUnauthorizedResponse($e);
         }
+    }
+
+    private function extractAuthToken(Request $request): string
+    {
+        $authToken = $request->getHeaderLine('Authorization');
+        return str_replace('Bearer ', '', $authToken);
+    }
+
+    private function handleUnauthorizedResponse(\Exception $e): Response
+    {
+        return $this->status_401(
+            ResponseTitle::UNAUTHORIZED,
+            ['Cannot retrieve resource' => $e->getMessage()],
+            'Kindly provide a valid bearer token'
+        );
     }
 }
