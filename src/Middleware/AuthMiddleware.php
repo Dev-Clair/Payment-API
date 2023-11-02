53bd93eb0d1fff5eda119a1dc3384ef566ebb420
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-11-02 12:03:55 +0100

    fix: modified auth middleware

--- src/Middleware/AuthMiddleware.php
+++ src/Middleware/AuthMiddleware.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 namespace Payment_API\Middleware;
 
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
+use Payment_API\Controller\AuthController;
 use Slim\Psr7\Response as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as Handler;
@@ -16,21 +15,25 @@ class AuthMiddleware
 {
     use Status_401;
 
+    public function __construct(private string $jwtSecretKey)
+    {
+    }
+
     public function __invoke(Request $request, Handler $handler): Response
     {
-        $secretKey = $_ENV['JWT_SECRET_KEY'];
-
         $authToken = $request->getHeaderLine('Authorization');
         $authToken = str_replace('Bearer ', '', $authToken);
 
         try {
-            $decodedToken = JWT::decode($authToken, new Key($secretKey, 'HS256'));
+            (new AuthController($this->jwtSecretKey))->decode($authToken);
             return $handler->handle($request);
         } catch (\Exception $e) {
             return $this->status_401(
                 ResponseTitle::UNAUTHORIZED,
-                'Cannot retrieve resource.',
-                'Kindly provide a valid bearer token.'
+                [
+                    'Cannot retrieve resource' => $e->getMessage()
+                ],
+                'Kindly provide a valid bearer token'
             );
         }
     }
