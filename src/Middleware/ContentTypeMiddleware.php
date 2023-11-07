ac2d608ec3d22684bcc2c412ac6c82b9e99c6299
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-11-07 08:44:41 +0100

    fix: status namespace import

--- src/Middleware/ContentTypeMiddleware.php
+++ src/Middleware/ContentTypeMiddleware.php
@@ -7,7 +7,7 @@ namespace Payment_API\Middleware;
 use Slim\Psr7\Response as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as Handler;
-use Payment_API\Utils\Response\Status_400 as Status_400;
+use Payment_API\Utils\Response\Status_400;
 use Payment_API\Enums\MiddlewareResponseTitle as ResponseTitle;
 
 class ContentTypeMiddleware
